9cdd285953cec335ca704a6d71f0e03571ac90cc
kenta-s <knt01222@gmail.com>
2017-08-26 16:08:00 +0900

    Set profiles when authorized

--- src/twcrl/rcfile_handler.cr
+++ src/twcrl/rcfile_handler.cr
@@ -1,39 +1,78 @@
 require "yaml"
 
 module Twcrl
-  class RcfileBuilder
-    YAML.mapping(
-      profiles: YAML::Any?,
-      configuration: YAML::Any?,
-    )
-  end
-
   class RcfileHandler
-    def initialize
-      # @config = {} of YAML::Type => YAML::Type
-      # @config = load_file
+    def initialize(@config : YAML::Any? = nil)
+    end
+
+    def profile
+      config["profiles"][screen_name][consumer_key]
+    end
+
+    def screen_name : String
+      name = config["configuration"]["default_profile"]["screen_name"]
+      name.to_s
+    end
+
+    def consumer_key : String
+      key = config["configuration"]["default_profile"]["consumer_key"]
+      key.to_s
+    end
+
+    def config
+      @config ||= load_file
+    end
+
+    def set_profile(screen_name : String, consumer_key : String, consumer_secret : String, access_token : String, access_token_secret : String)
+      yaml =<<-YAML
+      #{config["configuration"].to_yaml}
+
+      profiles:
+        #{screen_name}:
+          #{consumer_key}:
+            screen_name: #{screen_name}
+            consumer_key: #{consumer_key}
+            consumer_secret: #{consumer_secret}
+            access_token: #{access_token}
+            access_token_secret: #{access_token_secret}
+      YAML
+      @config = YAML.parse(yaml)
+      save
+    end
+
+    def set_default_profile(screen_name : String, consumer_key : String) : Void
+      yaml = config["configuration"]["default_profile"].as_h
+      yaml["screen_name"] = screen_name
+      yaml["consumer_key"] = consumer_key
+      save
     end
 
     def load_file
       if File.exists?(file_path)
-        RcfileBuilder.from_yaml(File.read(file_path))
+        YAML.parse(File.read(file_path))
       else
-        yaml = <<-YAML
+        yaml =<<-YAML
         ---
         profiles:
-          default_name:
+          default_screen_name:
             default_consumer_key:
+              screen_name:
+              consumer_key:
+              consumer_secret:
+              access_token:
+              access_token_secret:
+
         configuration:
           default_profile:
-          - default_name
-          - default_consumer_key
+            screen_name: default_screen_name
+            consumer_key: default_consumer_key
         YAML
-        RcfileBuilder.from_yaml(yaml)
+        YAML.parse(yaml)
       end
     end
 
     def save : Void
-      File.write(@file_name, config.to_yaml)
+      File.write(file_path, config.to_yaml)
     end
 
     def file_path : String
