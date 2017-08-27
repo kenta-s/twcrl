f1e16d7a4b9fe2cae9e7b80d6bf093770268c7eb
kenta-s <knt01222@gmail.com>
2017-08-27 13:39:07 +0900

    handle post request

--- src/twcrl/option_handler.cr
+++ src/twcrl/option_handler.cr
@@ -4,23 +4,38 @@ module Twcrl
   class OptionHandler
     getter consumer_key : String
     getter consumer_secret : String
+    getter option_parser : OptionParser?
+
+    property http_method : String
 
     def initialize
       args = ARGV
-      command = args.shift
       @consumer_key = ""
       @consumer_secret = ""
+      @http_method = "get"
+      @option_parser = nil
+      @params = ""
+
+      if args.any?
+        command = args.shift
+      else
+        command = ""
+      end
 
       parse(args)
+
       if command == "authorize"
+        parse(args)
         authorize
+      elsif /\/.*/.match(command)
+        call_api(command)
       else
-        get(command)
+        puts @option_parser
       end
     end
 
     def parse(args)
-      OptionParser.parse(args) do |parser|
+      @option_parser = OptionParser.parse(args) do |parser|
         parser.banner = <<-USAGE
         Usage: twcrl authorize --consumer-key key --consumer-secret secret
                twcrl [options] /1.1/statuses/home_timeline.json
@@ -28,9 +43,19 @@ module Twcrl
         parser.on("-c key", "--consumer-key key", "consumer key") do |key|
           @consumer_key = key
         end
+
         parser.on("-s secret", "--consumer-secret secret", "consumer secret") do |secret|
           @consumer_secret = secret
         end
+
+        parser.on("-x method", "--request-method method", "HTTP method(default: GET)") do |method|
+          @http_method = method
+        end
+
+        parser.on("-d data", "--data data", "parameters used in a POST request") do |data|
+          @params = data
+        end
+
         parser.on("-h", "--help", "Show this help") { puts parser }
       end
     end
@@ -46,12 +71,20 @@ module Twcrl
       end
     end
 
-    def get(path)
-      # path = "/1.1/followers/ids.json"
+    def call_api(path)
       access_token = OAuth::AccessToken.new(rc.access_token, rc.access_token_secret)
       consumer.authenticate(client, access_token)
-      response = client.get(path)
-      puts response.body
+      case @http_method
+      when "get"
+        response = client.get(path)
+        puts response.body
+      when "post"
+        # TODO: make sure this works
+        response = client.post_form(path, @params)
+        puts response.body
+      else
+        raise "Invalid HTTP method: #{@http_method}"
+      end
     end
 
     def rc
